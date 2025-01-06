5c9039a6a8bd58f3b024bcdd7fefb8bf2dba458a
kinako825 <snowberries@gmail.com>
2025-01-06 21:31:46 +0900

    jsの練習

---
+++ js-easy/js-basic/main.js
@@ -0,0 +1,28 @@
+// いろいろな例
+console.log('Hello, World!')
+console.log('こんにちは世界！')
+console.log(1 + 2)
+console.log(10 - 4 * 8)
+
+const answer = 42;
+console.log(answer)
+
+// var
+var a = 0;
+var a = 1;
+console.log(a)
+
+// let
+let b = 0;
+// let b = 1; //エラーになる
+
+// const
+const c = 0;
+// const c = 1; //エラーになる
+
+//文字列結合
+const breakfast = "お茶漬け"
+console.log("今日の朝ごはんは" + breakfast + "でした。")
+
+const breakfast1 = "納豆ご飯"
+console.log(`今日の朝ごはんは${breakfast1}でした。`)
