6fca2d1a85dbbd3a6f7f543f00795a2e624192a5
kinako825 <snowberries@gmail.com>
2025-01-11 16:28:29 +0900

    jsの練習

---
+++ js-easy/saitosan/main.js
@@ -0,0 +1,16 @@
+
+
+// const setBtn = funnction() 
+// {
+//     const hoge = Math.random() * 10;
+//     console.log('hoge');
+// };
+
+const setBtn = function () {
+    const hoge = Math.random() * 10;
+    if (hoge < 5) {
+        alert('ぺっぺっぺー');
+    } else {
+        alert('斎藤さんだぞ');
+    }
+};
