c02269a1b1db4939d3d47d22e23a09727814049f
kinako825 <snowberries@gmail.com>
2025-01-18 22:28:51 +0900

    jsの練習

--- js-normal/hit-timer/main.js
+++ js-normal/hit-timer/main.js
@@ -2,19 +2,20 @@ const startBtn = document.getElementById('startTimer');
 const confirmBtn = document.getElementById('confirmTime');
 let count = 0;
 let timer;
+let timeout;
 
 startBtn.addEventListener('click', function () {
     timer = setInterval(function () { count++; console.log(count); }, 1000);
-
-    setTimeout(function () {
+    timeout = setTimeout(function () {
         clearInterval(timer);
         alert('負け！');
     }, 40000);
-    // clearInterval(timer);
-    // count = 0;
+    count = 0;
 });
 
 confirmBtn.addEventListener('click', function () {
+    clearInterval(timer);
+    clearInterval(timeout);
     if (count === 20) {
         alert('20秒ジャスト！すごい');
     } else if (count < 20) {
@@ -22,10 +23,44 @@ confirmBtn.addEventListener('click', function () {
     } else {
         alert('もう' + count + '秒だよ…');
     }
-    clearInterval(timer);
     count = 0;
 });
 
 
+// チャレンジ問題
+// let timer;
 
+// const confirmTime2 = document.getElementById("confirmTime2");
+// const startTimer2 = document.getElementById("startTimer2");
 
+// let startTime;
+// let nowTime;
+// let diffTime;
+
+// confirmTime2.addEventListener("click", function () {
+//     nowTime = new Date();
+//     if (startTime === undefined) {
+//         return;
+//     }
+//     if (diffTime === 20) {
+//         alert("大正解^-^");
+//     } else if (diffTime < 20) {
+//         alert(`まだ${diffTime}秒、、、再スタートだ`);
+//     } else {
+//         alert(`もう${diffTime}秒だ！再挑戦せよ`);
+//     }
+//     clearInterval(timer);
+// });
+
+// startTimer2.addEventListener("click", function () {
+//     startTime = new Date();
+//     timer = setInterval(countUp2, 1000);
+// });
+
+// const countUp2 = function () {
+//     let checkTime = new Date();
+//     diffTime = Math.floor((checkTime.getTime() - startTime.getTime()) / 1000);
+//     if (diffTime === 40) {
+//         alert("終了/また挑戦しろ");
+//     }
+// };
