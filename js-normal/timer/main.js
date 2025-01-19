0cc8ac0b192a32fa9f40baadd59abf40abc6c666
kinako825 <snowberries@gmail.com>
2025-01-19 11:39:20 +0900

    jsの練習

---
+++ js-normal/timer/main.js
@@ -0,0 +1,44 @@
+const inputTime = document.getElementById('inputTime');
+const setTime = document.getElementById('setTime');
+const startTime = document.getElementById('startTimer');
+const stopTime = document.getElementById('stopTimer');
+let count = 0;
+let timerId = null; //setIntervalの結果を格納する変数
+let Originalcount = 0; //最初に設定した数字を保持しておく
+
+setTime.addEventListener('click', function () {
+    const inputValue = Number(inputTime.value); //入力された数字を変数に代入
+    count = inputValue;
+    Originalcount = inputValue;
+    console.log(count);
+});
+
+startTime.addEventListener('click', function () {
+    if (timerId === null) {
+        timerId = setInterval(function () {
+            count--;
+            console.log(count);
+            if (count === 0) {
+                clearInterval(timerId);
+                timerId = null;
+                count = Originalcount;
+                alert('タイマー終了');
+            }
+        }, 1000);
+    } else {
+        alert("タイマーは動作中です");
+    }
+
+});
+
+stopTime.addEventListener('click', function () {
+    if (timerId !== null) {
+        clearInterval(timerId);
+        timerId = null;
+        alert("タイマーを停止");
+    } else {
+        alert("タイマーは動作していません");
+    }
+
+});
+
