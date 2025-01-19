62bd8dcd0766975c6a6bc6158374e27f461c0a1e
kinako825 <snowberries@gmail.com>
2025-01-19 11:48:03 +0900

    jsの練習

--- js-normal/timer/main.js
+++ js-normal/timer/main.js
@@ -2,7 +2,7 @@ const inputTime = document.getElementById('inputTime');
 const setTime = document.getElementById('setTime');
 const startTime = document.getElementById('startTimer');
 const stopTime = document.getElementById('stopTimer');
-let count = 0;
+let count = 10;
 let timerId = null; //setIntervalの結果を格納する変数
 let Originalcount = 0; //最初に設定した数字を保持しておく
 
