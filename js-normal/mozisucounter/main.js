f9da428bc24ef899153703e4715466eb1c11b12b
kinako825 <snowberries@gmail.com>
2025-01-22 20:49:49 +0900

    jsの練習

--- js-normal/mozisucounter/main.js
+++ js-normal/mozisucounter/main.js
@@ -1,14 +1,34 @@
 
 
-const SampleForm = document.getElementById('sampleForm');
-const TextCounter = document.getElementById('textCounter');
+const sampleForm = document.getElementById('sampleForm');
+const textCounter = document.getElementById('textCounter');
+const resetBtn = document.getElementById('resetBtn');
+const originalMozisu = 400
+let mozisuCount = 0;
 
 
-SampleForm.addEventListener('keyup', function () {
-    const length = SampleForm.value.length;
-    let mozisu = length;
-    console.log(400 - mozisu);
-    TextCounter.innerText = mozisu;
+const wordCount = () => {
+    const remainingNumber = originalMozisu;
+    const mozisuCount = (remainingNumber - sampleForm.value.length);
+    console.log(mozisuCount);
+    textCounter.innerText = ('残り' + mozisuCount + '文字');
 
+}
+
+const init = () => {
+    mozisuCount = 0;
+    wordCount();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    wordCount();
 });
 
+sampleForm.addEventListener('keyup', function () {
+    wordCount();
+});
+
+resetBtn.addEventListener('click', function () {
+    init();
+    console.log(mozisuCount);
+});
