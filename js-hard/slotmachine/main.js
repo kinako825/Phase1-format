b487ab476b02a432955dd947325b153c9929fa73
kinako825 <snowberries@gmail.com>
2025-01-30 21:04:34 +0900

    jsの練習

---
+++ js-hard/slotmachine/main.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById('setTime1');
+const button2 = document.getElementById('setTime2');
+const button3 = document.getElementById('setTime3');
+
+button1.addEventListener('click', function () {
+    document.getElementById('setTime1').disabled = true;
+
+
+});
