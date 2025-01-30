209ea86a28c89c0d59c51f426b3b2b7e16ec5e0d
kinako825 <snowberries@gmail.com>
2025-01-30 20:40:34 +0900

    jsの練習

--- js-hard/ToDo/main.js
+++ js-hard/ToDo/main.js
@@ -1,32 +1,31 @@
 
-
-
 const addBtn = document.querySelector('.add-btn');
-
 const ul = document.querySelector('.lists');
-
-
-
 
 
 addBtn.addEventListener('click', function () {
     const textArea = document.getElementById('add-area');
     let todoText = textArea.value;
-    const li = document.createElement('li');
-    li.textContent = todoText;
 
-    const doneBtn = document.createElement('buttun');
-    doneBtn.textContent = "完了";
-    doneBtn.classList.add("done-btn");
+    if (textArea.value === "") {
+        alert("テキストを入力してください");
+    } else {
 
-    doneBtn.addEventListener('click', function () {
-        li.remove();
-    });
+        const li = document.createElement('li');
+        li.textContent = todoText;
 
-    ul.appendChild(li);
-    li.appendChild(doneBtn);
+        const doneBtn = document.createElement('button');
+        doneBtn.textContent = "完了";
 
-    textArea.value = "";
+        doneBtn.addEventListener('click', function () {
+            li.remove();
+        });
+
+        ul.appendChild(li);
+        li.appendChild(doneBtn);
+
+        textArea.value = "";
+    }
 
 
 });
