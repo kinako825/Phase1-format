d251802712dcaa3035442b72d5c0fff1095b77d5
kinako825 <snowberries@gmail.com>
2025-02-11 11:41:09 +0900

    jsの練習

--- js-hard/howmanydays/main.js
+++ js-hard/howmanydays/main.js
@@ -33,27 +33,25 @@ text1.textContent = `まで後日時間分秒`;
 document.body.appendChild(text1);
 
 
+
 function countdown(date) {
     let eventDate = moment(date, "YYYY-MM-DD");
     let now = moment();
 
-    let diffSeconds = eventDate.diff(now, "seconds"); // 総秒数を取得する
+    let remainingTime = moment.duration(eventDate.diff(now)); // 残り時間の差を取得
 
-    if (diffSeconds < 0) {
+    let days = remainingTime.days();
+    let hours = remainingTime.hours();
+    let minutes = remainingTime.minutes();
+    let seconds = remainingTime.seconds();
+
+    if (remainingTime.asSeconds() < 0) {
         text1.textContent = "カウントダウン終了";
-        return;
+    } else {
+        text1.textContent = `${countdownText.value} まで後 ${days}日 ${hours}時間 ${minutes}分 ${seconds}秒`;
     }
+}
 
-    // let remainingTime = moment.duration(eventDate.diff(now));
-
-    let days = Math.floor(diffSeconds / (60 * 60 * 24)); // 日数
-    let hours = Math.floor((diffSeconds % (60 * 60 * 24)) / (60 * 60)); // 時間
-    let minutes = Math.floor((diffSeconds % (60 * 60)) / 60); // 分
-    let seconds = diffSeconds % 60; // 秒
-
-    text1.textContent = countdownText.value + `まで後${days}日${hours}時間${minutes}分${seconds}秒`;
-
-};
 
 dateSearchBtn.addEventListener('click', function () {
     const date = countdownText.value;
